9da78ff010f89305edf22d196aae1b263ca7e583
Drew Hopkins <hopkins199@gmail.com>
2021-05-17 15:42:44 -0700

    massive update of projects

--- level-two/es6-practice/main.js
+++ level-two/es6-practice/main.js
@@ -56,6 +56,68 @@ const people = [
     })
 }*/
 
-friendlyPeople = arr => arr.filter(peoples => (peoples.friendly))
+let friendlyPeople = people.filter(person => person.friendly)
 
 console.log(friendlyPeople)
+
+// 4 re-write in es6
+
+function doMathSum(a, b) {
+    return a + b
+}
+
+let doMathSum = (a,b) => a + b
+
+var produceProduct = function(a, b) {
+    return a * b
+}
+
+let produceProduct = (a,b) => a * b
+
+//5 Write a printString function that takes firstName, lastName, and age as parameters and returns a string like the following:
+
+// Hi Kat Stark, how does it feel to be 40?
+
+let firstName = "Jane"
+let lastName = "Doe"
+let age = "100"
+
+//console.log(`Hi ${firstName}, ${lastName}, how does it feel to be ${age}`)
+
+// 6  Use the shorthand syntax to make the following filter take up one line. Copy and paste the array to test it.
+
+const animals = [
+    {
+        type: "dog",
+        name: "theodore"
+    },
+    {
+        type: "cat",
+        name: "whiskers"
+    },
+    {
+        type: "pig",
+        name: "piglette"
+    },
+    {
+        type: "dog",
+        name: "sparky"
+    }
+ ];
+ 
+ /* function filterForDogs(arr) {
+     return arr.filter(animal => {
+         if (animal.type === "dog") {
+             return true
+         } else {
+             return false
+         }
+     })
+ } */
+
+ let filterForDogs = animals.filter(animal => animal.type === "dog")
+
+ console.log(filterForDogs)
+
+
+ 

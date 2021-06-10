63dc335c702bc59317eba8d4ab83555b82be1b56
Drew Hopkins <hopkins199@gmail.com>
2021-06-10 15:04:56 -0700

    meme generator added

--- level-three/meme-generator/src/App.js
+++ level-three/meme-generator/src/App.js
@@ -1,19 +1,108 @@
 import React from "react"
-import Gen from "./Gen"
+import axios from "axios"
+import Meme from "./Meme"
+import SavedMeme from './SavedMeme'
 
-class App extends React.Component {
+class App extends React.Component{
+    constructor() {
+        super()
 
+        this.state = {
+            memes: [],
+            savedMemes: [],
+        }
+        this.saveMeme = this.saveMeme.bind(this)
+        this.refreshClick = this.refreshClick.bind(this)
+        this.deleteMeme = this.deleteMeme.bind(this)
+        this.editMeme = this.editMeme.bind(this)
+    }
 
+    componentDidMount() {
+        //Get Meme API
+        axios.get("https://api.imgflip.com/get_memes")
+        .then(res => res.data)
+        .then(res => {
+            this.setState(
+                {
+                    memes: [...res.data.memes],
+                }
+            )
+        })
+    }
 
+    saveMeme(newMeme) {
+        //Function passed to Save button inside Meme Component, pushes Meme State into savedMemes arr
+            this.setState(prevState => {
+                return {
+                savedMemes: [...prevState.savedMemes, newMeme],
+                }   
+            })
+    }
 
+    deleteMeme(id) {
+        //Deletes saved meme by Id
+        const deletedMeme = this.state.savedMemes.filter(meme => meme.id !== id);
+        // console.log(deletedMeme)
+        this.setState(prevState => {
+            return {
+                savedMemes: [...deletedMeme]
+            }
+        })
+    }
 
-    render(){
+    editMeme(id, editedMeme) {
+        const edits = this.state.savedMemes.map(meme => {
+            if(meme.id === id) {
+                meme.bottomText = editedMeme.bottomText
+                meme.topText = editedMeme.topText
+                return meme
+            } 
+            return meme
+        })
 
+        // console.log(edits)
+            
+        this.setState(prevState => {
+            return {
+                savedMemes: prevState.savedMemes = [...edits]
+            }
+        })
+    }
+
+    refreshClick(event){
+        event.preventDefault()
+        axios.get("https://api.imgflip.com/get_memes")
+        .then(res => res.data)
+        .then(res => {
+            this.setState(
+                {
+                    memes: [...res.data.memes],
+                }
+            )
+        })
+    }  
+
+    render() {
+        const memesArr = this.state.memes.map(meme => <Meme key={Number(Math.floor(Math.random() * 1000000))} id={meme.id} name={meme.name} img={meme.url} save={this.saveMeme}/>)
+        const savedMemes = this.state.savedMemes.map(meme => <SavedMeme key={Number(Math.floor(Math.random() * 1000000))} id={meme.id} name={meme.name} img={meme.imgUrl} topText={meme.topText} bottomText={meme.bottomText} delete={this.deleteMeme} edit={this.editMeme} />)
+        const randomMeme = memesArr[Math.floor(Math.random() * memesArr.length)]
+        // console.log(this.state.savedMemes);
+        
         return(
-            <div>
-                <Gen />
-            </div>
+            <main>
+                <div>
+                    {randomMeme}
+                    <div className="refresh-div">
+                        <button className="button" onClick={this.refreshClick}>New Meme</button>
+                    </div>
+                </div>
+
+                <div>
+                    {savedMemes}
+                </div>
+            </main>
         )
+        
     }
 }
 
--- level-three/meme-generator/src/Meme.js
+++ level-three/meme-generator/src/Meme.js
@@ -1,48 +1,52 @@
 import React from "react"
 
 class Meme extends React.Component {
-    constructor() {
+    constructor(props) {
         super()
         this.state = {
+            name: props.name,
+            imgUrl: props.img,
             topText: "",
-            bottomText: ""
+            bottomText: "",
+            id: props.id
         }
         this.handleChange = this.handleChange.bind(this)
     }
 
     handleChange(e) {
+        //Generates Preview when typing inside of inputs
         const {name, value} = e.target
         this.setState({[name]: value})
     }
-    render(){
+
+    render() {
         return (
-            <div>
+            <div className="outer-container">
                 <div className="meme-container">
-                    <h1 className="upper-text">{this.state.topText}</h1>
-                    <img className="meme-img" src={this.props.img} alt={this.props.name} width="500px" height="500px"/>
-                    <h1 className="lower-text">{this.state.bottomText}</h1>
+                    <h1 className="upper">{this.state.topText}</h1>
+                    <img className="img" src={this.state.imgUrl} alt={this.state.name} width="500px" height="500px"/>
+                    <h1 className="lower">{this.state.bottomText}</h1>
                 </div>
                 <form>
-                        <input
-                            type="text"
-                            name="topText"
-                            value={this.state.topText}
-                            placeholder="Top Text"
-                            size="30"
-                            onChange={this.handleChange}
-                        />
-                        <input
-                            type="text"
-                            name="bottomText"
-                            value={this.state.bottomText}
-                            placeholder="Bottom Text"
-                            size="31"
-                            onChange={this.handleChange}
-                        />
-                    </form>
+                    <input
+                        type="text"
+                        name="topText"
+                        value={this.state.topText}
+                        placeholder="Top Text"
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        type="text"
+                        name="bottomText"
+                        value={this.state.bottomText}
+                        placeholder="Bottom Text"
+                        onChange={this.handleChange}
+                    />
+                </form>
+                <button className="button" onClick={() => {this.props.save(this.state)}}>Save Meme</button>
             </div>
         )
-    }
+    } 
 }
 
 export default Meme
